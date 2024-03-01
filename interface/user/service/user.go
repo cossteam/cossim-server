a1b4fa24f07086b92f168e2b02ad1f7318006126
zzw <2318266924@qq.com>
2024-03-01 15:27:15 +0800

    fix(relation_service):修复已读公告后再次已读会添加新记录

--- interface/user/service/user.go
+++ interface/user/service/user.go
@@ -187,7 +187,7 @@ func (s *Service) Register(ctx context.Context, req *model.RegisterRequest) (str
 	key := myminio.GenKey(bucket, fileID+".jpeg")
 	headerUrl, err := s.sp.Upload(ctx, key, reader, reader.Size(), minio.PutObjectOptions{
 		ContentType: "image/jpeg",
-		Expires:     ostime.Now().AddDate(10, 0, 0),
+		Expires:     ostime.Now().AddDate(0, 0, 7),
 	})
 	if err != nil {
 		return "", err
