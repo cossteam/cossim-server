68716c42c4949423a4fcd95e54d3b27493dfdced
am6737 <91421697+am6737@users.noreply.github.com>
2024-03-28 09:33:46 +0800

    pref(relation): 优化获取好友关系查询条件 (#434)

--- internal/relation/infrastructure/persistence/user_relation_repository.go
+++ internal/relation/infrastructure/persistence/user_relation_repository.go
@@ -56,7 +56,7 @@ func (u *UserRelationRepo) GetRelationByIDs(userId string, friendIds []string) (
 
 func (u *UserRelationRepo) GetRelationsByUserID(userId string) ([]*entity.UserRelation, error) {
 	var relations []*entity.UserRelation
-	if err := u.db.Where("user_id = ? AND status = ? AND deleted_at = 0 AND friend_id NOT IN (?)", userId, entity.UserStatusNormal, constants.SystemUserList).Find(&relations).Error; err != nil {
+	if err := u.db.Where("user_id = ? AND status != ? AND deleted_at = 0 AND friend_id NOT IN (?)", userId, entity.UserStatusDeleted, constants.SystemUserList).Find(&relations).Error; err != nil {
 		return nil, err
 	}
 	return relations, nil
