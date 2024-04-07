de9b9347f83fb6430c19c6e030b4614c1aceef0f
am6737 <91421697+am6737@users.noreply.github.com>
2024-04-07 10:09:37 +0800

    deploy(compose): 优化compose启动参数 (#494)
    Co-authored-by: lms <lms@test.com>

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -29,12 +29,12 @@ func init() {
 	flag.BoolVar(&remoteConfig, "remote-config", false, "Load config from remote source")
 	flag.StringVar(&remoteConfigAddr, "config-center-addr", "", "Address of the config center")
 	flag.StringVar(&remoteConfigToken, "config-center-token", "", "Token for accessing the config center")
-	flag.BoolVar(&hotReload, "hot-reload", true, "Enable hot reloading")
+	flag.BoolVar(&hotReload, "hot-reload", false, "Enable hot reloading")
 	flag.StringVar(&configKey, "config-key", "service/gateway", "Service configuration path in the configuration center")
-	flag.StringVar(&pprofAddr, "pprof-bind-address", ":6060", "The address the pprof endpoint binds to")
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":9090", "The address the metric endpoint binds to")
-	flag.StringVar(&httpProbeAddr, "http-health-probe-bind-address", ":9091", "The address to bind the http health probe endpoint")
-	flag.StringVar(&grpcProbeAddr, "grpc-health-probe-bind-address", ":9092", "The address to bind the grpc health probe endpoint")
+	flag.StringVar(&pprofAddr, "pprof-bind-address", "0", "The address the pprof endpoint binds to")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metric endpoint binds to")
+	flag.StringVar(&httpProbeAddr, "http-health-probe-bind-address", "0", "The address to bind the http health probe endpoint")
+	flag.StringVar(&grpcProbeAddr, "grpc-health-probe-bind-address", "0", "The address to bind the grpc health probe endpoint")
 	flag.Parse()
 }
 
