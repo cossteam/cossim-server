f1a7f021238b217eb74bf41b494749af980bd361
oldtt <2622788078@qq.com>
2024-04-17 10:16:32 +0800

    fix:修复登录时last_login_time一直无变化

--- internal/user/interface/grpc/user_login_grpc.go
+++ internal/user/interface/grpc/user_login_grpc.go
@@ -2,7 +2,6 @@ package grpc
 
 import (
 	"context"
-	"fmt"
 	v1 "github.com/cossim/coss-server/internal/user/api/grpc/v1"
 	"github.com/cossim/coss-server/internal/user/domain/entity"
 	"github.com/cossim/coss-server/pkg/code"
@@ -78,7 +77,7 @@ func (s *UserServiceServer) GetUserLoginByDriverIdAndUserId(ctx context.Context,
 			return resp, status.Error(codes.Code(code.UserErrGetUserLoginByDriverIdAndUserIdFailed.Code()), err.Error())
 		}
 	}
-	fmt.Println("GetUserLoginByDriverIdAndUserId info => ", info)
+
 	if info != nil {
 		resp.UserId = info.UserId
 		resp.Token = info.Token
